86e592144e072a376bcae09ea56c1f43f141ba6a
Yashwanth Reddy Veesarapu <yashwanthreddyveesarapu@gmail.com>
2024-02-13 16:00:29 -0500

    Update environment.ts
    Bug

--- environment.ts
+++ environment.ts
@@ -1,6 +1,1 @@
-export const environment = {
-  token:
-    'WW91IG11c3QgYmUgdGhlIGN1cmlvdXMgdHlwZS4gIEJyaW5nIHRoaXMgdXAgYXQgdGhlIGludGVydmlldyBmb3IgYm9udXMgcG9pbnRzICEh',
 
-  apiURL: 'https://us-central1-crm-sdk.cloudfunctions.net/flightInfoChallenge',
-};
